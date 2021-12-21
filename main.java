21d90ccc80d6a461895b1e9dc2370ea5ab204d08
097Harsh <96407923+097Harsh@users.noreply.github.com>
2021-12-21 18:06:53 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,29 @@
+public class Main{
+	
+	public static void main(String [] args)
+		{
+			int num1=45 , num2=78;
+
+			System.out.print("The num1 + num2 is:");
+			System.out.println(num1 + num2);
+
+			System.out.print("Thw num1 - num2 is :");
+			System.out.println(num1 - num2);
+
+			System.out.print("The num1 * num2 is :");
+			System.out.println(num1 * num2);
+
+			System.out.print("The num1 / num2 is :");
+			System.out.println(num1 /num2);
+
+			System.out.print("The num1 % num2 is :");
+			System.out.println(num1 % num2);
+
+			System.out.println(++num1);
+			System.out.println(num1++);
+			System.out.println(num1--);
+			System.out.println(--num1);
+			
+		}
+		
+	}
